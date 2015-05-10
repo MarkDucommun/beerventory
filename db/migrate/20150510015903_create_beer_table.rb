40fa35acb315374eb7d5c88833b0782f062c3203
Mark Ducommun <mark@giveforward.com>
2015-05-09 21:27:48 -0500

    creates beer table

--- db/migrate/20150510015903_create_beer_table.rb
+++ db/migrate/20150510015903_create_beer_table.rb
@@ -1,6 +1,11 @@
 class CreateBeerTable < ActiveRecord::Migration
   def change
-    create_table :beer_tables do |t|
+    create_table :beers do |t|
+      t.string :name, null: false
+      t.datetime :purchase_date
+      t.datetime :bottling_date
+      t.datetime :best_by_date
+      t.timestamps
     end
   end
 end
