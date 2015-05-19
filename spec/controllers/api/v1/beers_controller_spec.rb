0ec85084db84e0b44f517f00d0826eb2841ce9bc
Mark Ducommun <mark@giveforward.com>
2015-05-19 18:55:27 -0500

    adds index, update and destroy to beer controller

--- spec/controllers/api/v1/beers_controller_spec.rb
+++ spec/controllers/api/v1/beers_controller_spec.rb
@@ -1,6 +1,15 @@
 require 'rails_helper'
 
 describe Api::V1::BeersController do
+  describe '#index' do
+    it 'returns all beer records' do
+      beers = create_list(:beer, 2)
+      get :index
+      beers_hash = parse_json_response_body(response).fetch(:beers)
+      expect(beers_hash.length).to be beers.length
+    end
+  end
+
   describe '#create' do
     let(:name) { 'Beer' }
     let(:brewery) { create(:brewery) }
@@ -23,6 +32,32 @@ describe Api::V1::BeersController do
       expect(beer_hash[:id]).to_not be nil
       expect(beer_hash[:name]).to eq name
       expect(beer_hash[:brewery][:id]).to be brewery.id
+    end
+  end
+
+  describe '#update' do
+    it 'updates the beer record and returns the updated beer record' do
+      beer = create(:beer)
+      new_brewery_id = create(:brewery).id
+      put :update, id: beer.id, beer: {brewery_id: new_brewery_id}
+      beer_hash = parse_json_response_body(response).fetch(:beer)
+      expect(beer_hash[:brewery][:id]).to eq new_brewery_id
+    end
+  end
+
+  describe '#destroy' do
+    let!(:beer) { create(:beer) }
+
+    it 'destroys the beer record' do
+      expect {
+        delete :destroy, id: beer.id
+      }.to change(Beer, :count).by(-1)
+    end
+
+    it 'returns the destroyed beer record' do
+      delete :destroy, id: beer.id
+      beer_hash = parse_json_response_body(response).fetch(:beer)
+      expect(beer_hash[:id]).to eq beer.id
     end
   end
 end
